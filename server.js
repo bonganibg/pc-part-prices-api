26d2e50b89d1f0e559e8435caab129ab4bcd0ee2
Bonganibg <bongani.gumbo@yahoo.com>
2022-02-21 17:59:49 +0200

    Heroku test 3

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = require('./models/app');
 
 const server = http.createServer(app);
 
-const port = (process.env.port || 8080);
+const port = (process.env.PORT || 8080);
 app.set("port", port);
 app.listen(port);
-server.listen(port);
